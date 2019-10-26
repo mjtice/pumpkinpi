64ea22aa7de176cee810d26eb313ac6f3dedf6f0
gizzardqueen <mjtice@users.noreply.github.com>
2019-10-26 08:42:29 -0600

    Rewrite to make it more pythonic.

--- pumpkin.py
+++ pumpkin.py
@@ -1,89 +1,202 @@
-#!/usr/bin/env python2.7
-
 import RPi.GPIO as GPIO
-import signal
 from time import sleep
 import random
+import threading
+import math
+import logging
+import sys
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM) ## Indicates which pin numbering configuration to use
 
-ledEye = [27,22]
-btnNose = 23
-ledMouth = 24
-horn = 25
+class Pumpkin(object):
+    def __init__(self):
+        # setup logging
+        self.log = logging.getLogger()
+        self.log.setLevel(logging.DEBUG)
 
-GPIO.setup(ledMouth, GPIO.OUT)
-GPIO.output(ledMouth,GPIO.HIGH)
-GPIO.setup(ledEye, GPIO.OUT)
-GPIO.setup(btnNose, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(horn, GPIO.OUT)
-GPIO.output(horn,GPIO.HIGH)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.log.addHandler(handler)
 
-led = GPIO.PWM(ledMouth, 100)
+        # Initialize all the GPIO pins
+        self.gpio_pins = {
+            "eye_left": 27,
+            "eye_right": 22,
+            "nose": 23,
+            "mouth": 24,
+            "horn": 25,
+            "candle_red": 19,
+            "candle_green": 26
+        }
+        for i in self.gpio_pins.items():
+            self.log.debug("{} is assigned pin {}".format(i[0], i[1]))
 
-led.start(0)         
-pause_time = 0.02
-state = 1
+        self.log.debug("GPIO pins assigned, setting up GPIO.")
 
-# Blink Rate http://www.ncbi.nlm.nih.gov/pubmed/9399231
-# Blink once every 3.5 seconds (more or less)
-blinkRate = 3.529
-counter = 0
-GPIO.output(ledEye,GPIO.HIGH)
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)  ## Indicates which pin numbering configuration to use
 
-def signal_term_handler(signal, frame):
-    GPIO.cleanup()
+        GPIO.setup(self.gpio_pins['eye_left'], GPIO.OUT)
+        GPIO.setup(self.gpio_pins['eye_right'], GPIO.OUT)
+        GPIO.setup(self.gpio_pins['nose'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.gpio_pins['mouth'], GPIO.OUT)
+        GPIO.setup(self.gpio_pins['horn'], GPIO.OUT)
+        GPIO.setup(self.gpio_pins['candle_red'], GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.gpio_pins['candle_green'], GPIO.OUT, initial=GPIO.LOW)
 
-def blink():
-    randomNumber = random.uniform(.1,.3)
-    GPIO.output(ledEye,GPIO.LOW)
-    sleep(randomNumber)
-    GPIO.output(ledEye,GPIO.HIGH)
+        self.log.debug("GPIO setup.  Setting OUTPUT for horn, mouth, eye_left, eye_right")
+        GPIO.output(self.gpio_pins['horn'], GPIO.LOW)
+        GPIO.output(self.gpio_pins['mouth'], GPIO.HIGH)
+        GPIO.output(self.gpio_pins['eye_left'], GPIO.HIGH)
+        GPIO.output(self.gpio_pins['eye_right'], GPIO.HIGH)
 
-    randomInt = random.uniform(1, 10)
-    if (randomInt >= 6):
-        randomNumber = random.uniform(.1,.2)
-        GPIO.output(ledEye,GPIO.LOW)
-        sleep(randomNumber)
-        GPIO.output(ledEye,GPIO.HIGH)
+        self.log.debug("Enabling PWM on mouth, candle_red, candle_green")
+        self.pwm_mouth = GPIO.PWM(self.gpio_pins['mouth'], 100)
+        self.pwm_candle_red = GPIO.PWM(self.gpio_pins['candle_red'], 300)
+        self.pwm_candle_green = GPIO.PWM(self.gpio_pins['candle_green'], 300)
+        self.intensity = 1.0
 
-try:
-    while True:
-        if (counter >= blinkRate):
-            randomNumber = random.uniform(0,.2)
-            sleep(randomNumber)
-            blink()
-            counter = 0
+        self.pwm = [self.pwm_mouth, self.pwm_candle_red, self.pwm_candle_green]
 
-        ## When state toggle button is pressed
-        if ( GPIO.input(btnNose) == False ):
-            GPIO.output(ledEye,GPIO.HIGH)
-            for x in range(0,1):
-                for i in range(0,101):      # 101 because it stops when it finishes 100
-                    led.ChangeDutyCycle(i)
-                    sleep(pause_time)
-                    print(i)
-                for i in range(100,-1,-1):      # from 100 to zero in steps of -1
-                    led.ChangeDutyCycle(i)
-                    sleep(pause_time)
-                    print(i)
+        # Start the pins
+        self.log.debug("Starting PWM on mouth, candle_red, candle_green")
+        self.pwm_mouth.start(0)
+        self.pwm_candle_red.start(100)
+        self.pwm_candle_green.start(100)
 
-            sleep(2)
-            GPIO.output(horn,GPIO.LOW)
-            print "Honk!"
-            sleep(.3)
-            GPIO.output(horn,GPIO.HIGH)
-        
-        counter = counter + .001
-        signal.signal(signal.SIGTERM, signal_term_handler)
-        sleep(.001)
+        threads = [
+            threading.Thread(target=self._red_light, args=(self.pwm_candle_red, self.intensity)),
+            threading.Thread(target=self._green_light, args=(self.pwm_candle_red, self.intensity)),
+        ]
 
-except KeyboardInterrupt:
-    led.stop()
-    GPIO.cleanup()
-    #streamer.close()
+        for t in threads:
+            self.log.debug("Starting thread...")
+            t.daemon = True
+            t.start()
+            self.log.debug("Thread started")
+        # for t in threads:
+        #     self.log.debug("Joining threads...")
+        #     t.join()
+        #     self.log.debug("Thread joined")
 
-except SystemExit:
-    led.stop()
-    GPIO.cleanup()
+    def _red_light(self, candle, intensity):
+        self.log.debug("Starting red_candle thread")
+        candle.start(100)
+        while True:
+            candle.ChangeDutyCycle(min(random.randint(75, 100) * math.pow(intensity + 0.1, 0.75), 100))
+            self._rand_flicker_sleep()
+
+    def _green_light(self, candle, intensity):
+        self.log.debug("Starting green_candle thread")
+        candle.start(100)
+        while True:
+            candle.ChangeDutyCycle(random.randint(33, 44) * math.pow(intensity, 2))
+            self._rand_flicker_sleep()
+
+    def burning_down(self):
+        self.log.debug("Starting candle burn down")
+        while self.intensity > 0:
+            self.intensity = max(self.intensity - .01, 0)
+            self.pwm_candle_red.ChangeDutyCycle(self.intensity)
+            self.pwm_candle_green.ChangeDutyCycle(self.intensity)
+            sleep(.05)
+        self.intensity = 1.0
+        self.log.debug("Candle burn down complete")
+
+    def mouth(self):
+        self.log.debug("Starting mouth cycle")
+        pause_time = 0.02
+
+        for i in range(0, 101):  # 101 because it stops when it finishes 100
+            self.pwm_mouth.ChangeDutyCycle(i)
+            sleep(pause_time)
+        for i in range(100, -1, -1):  # from 100 to zero in steps of -1
+            self.pwm_mouth.ChangeDutyCycle(i)
+            sleep(pause_time)
+        self.log.debug("Mouth cycle complete")
+
+    def horn(self):
+        self.log.debug("Starting horn cycle")
+
+        sleep(2)
+        GPIO.output(self.gpio_pins['horn'], GPIO.HIGH)
+        sleep(.3)
+        GPIO.output(self.gpio_pins['horn'], GPIO.LOW)
+        self.log.debug("Horn cycle complete")
+
+    def _rand_flicker_sleep(self):
+        sleep(random.randint(3, 10) / 100.0)
+
+    def blink(self):
+        self.log.debug("Starting blink cycle")
+        random_number = random.uniform(.1, .3)
+        GPIO.output(self.gpio_pins['eye_left'], GPIO.LOW)
+        GPIO.output(self.gpio_pins['eye_right'], GPIO.LOW)
+
+        sleep(random_number)
+        GPIO.output(self.gpio_pins['eye_left'], GPIO.HIGH)
+        GPIO.output(self.gpio_pins['eye_right'], GPIO.HIGH)
+
+        random_int = random.uniform(1, 10)
+        if random_int >= 6:
+            random_number = random.uniform(.1, .2)
+            GPIO.output(self.gpio_pins['eye_left'], GPIO.LOW)
+            GPIO.output(self.gpio_pins['eye_right'], GPIO.LOW)
+
+            sleep(random_number)
+            GPIO.output(self.gpio_pins['eye_left'], GPIO.HIGH)
+            GPIO.output(self.gpio_pins['eye_right'], GPIO.HIGH)
+        self.log.debug("Blink cycle complete")
+
+
+def main():
+    pumpkin = None
+    try:
+        # Blink Rate http://www.ncbi.nlm.nih.gov/pubmed/9399231
+        # Blink once every 3.5 seconds (more or less)
+        blink_rate = 3.529
+
+        counter = 0
+        pumpkin = Pumpkin()
+
+        while True:
+            if counter >= blink_rate:
+                random_number = random.uniform(0, .2)
+                sleep(random_number)
+                pumpkin.blink()
+                counter = 0
+
+            # When state toggle button is pressed
+            if GPIO.input(pumpkin.gpio_pins['nose']) == GPIO.HIGH:
+                pumpkin.log.debug("Nose was pushed...")
+                # Turn the eyes on
+                GPIO.output(pumpkin.gpio_pins['eye_left'], GPIO.HIGH)
+                GPIO.output(pumpkin.gpio_pins['eye_right'], GPIO.HIGH)
+
+                # Dim the candle
+                pumpkin.burning_down()
+
+                # Cycle mouth
+                pumpkin.mouth()
+                sleep(2)
+
+                # Honk the horn
+                pumpkin.horn()
+            counter = counter + .001
+
+            # Artificial sleep to capture the button
+            sleep(.001)
+
+    except KeyboardInterrupt:
+        pass
+    except SystemExit:
+        pass
+    finally:
+        for p in pumpkin.pwm:
+            p.stop()
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
